3634c41654d4c910822cde56239e81ba1e41bcd3
repjarms <jeffarn12@gmail.com>
2017-08-31 20:27:28 -0400

    Change size of TWI TX to accept uint8_t array

--- twi.c
+++ twi.c
@@ -20,7 +20,8 @@
 static const nrf_drv_twi_t m_twi = NRF_DRV_TWI_INSTANCE(TWI_INSTANCE_ID);
 
 /* Location read from slave */
-static uint8_t m_location;
+static uint8_t m_location[3];
+static uint8_t arr_size = sizeof(m_location) / sizeof(m_location[0]);
 
 static volatile bool twi_xfer_done = false;
 
@@ -39,7 +40,7 @@ void twi_handler(nrf_drv_twi_evt_t const * p_event, void * p_context)
     case NRF_DRV_TWI_EVT_DONE:
       if (p_event->xfer_desc.type == NRF_DRV_TWI_XFER_RX) // if event is receive
       {
-        data_handler(m_location);
+        data_handler(m_location[0]);
       }
       twi_xfer_done = true;
       break;
@@ -75,9 +76,10 @@ void twi_init(void)
 ret_code_t read_piece_data()
 {
   NRF_LOG_INFO("Read piece location\n");
+  NRF_LOG_INFO("Size of %d\n", sizeof(m_location));
   twi_xfer_done = false;
 
-  ret_code_t err_code = nrf_drv_twi_rx(&m_twi, TWI_SLAVE_ADDR, &m_location, sizeof(m_location));
+  ret_code_t err_code = nrf_drv_twi_rx(&m_twi, TWI_SLAVE_ADDR, &m_location[0], arr_size);
   APP_ERROR_CHECK(err_code);
 
   return err_code;
